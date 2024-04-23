96b686e8ca3f97730f9492c4c1814c3a144558af
leonelborgesdev <leonel03t@gmail.com>
2024-04-23 08:41:43 -0400

    ordenando carpetas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const routes = require("./routes/index.routes.js");
+const routes = require("./src/routes/index.routes.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
